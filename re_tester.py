7009d94e3fe63d503dd7f086a7f42ed4abac4700
Peter Thompson <peter@hoekwil.com>
2016-07-05 13:35:03 +0200

    05-07-16

--- re_tester.py
+++ re_tester.py
@@ -1,40 +1,30 @@
 import re
 
 # Regular expressions
-re1 = re.compile('(Message-ID(.*?\n)*X-FileName.*?\n)|(To:(.*?\n)*?Subject.*?\n)|(< (Message-ID(.*?\n)*.*?X-FileName.*?\n))')
-re2 = re.compile('<|>|(\s---(.*?\n)?.*?---\s)|(.*?:(\s|(.*?\s)|))|(\(\d+\))|(\s.*?\..*?\s)|(\s.*?\_.*?\s)|(\s.*?\-.*?\s)|(\s.*\/.*?\s)|(\s.*@.*?\s)|([\d\-\(\)\\\/\#\=]+(\s|\.))|(\n.*?\s)|\d')
-re3 = re.compile('\s.\s|\s+')
+re1 = re.compile('(Message-ID(.*?\n)*X-FileName.*?\n)|'
+                 '(To:(.*?\n)*?Subject.*?\n)|'
+                 '(< (Message-ID(.*?\n)*.*?X-FileName.*?\n))')
+re2 = re.compile('.+@.+') # Remove emails
+re3 = re.compile('\s(---------------)(.*?)(---------------)\s', re.DOTALL)
+re4 = re.compile('''\s(\*\*\*\*\*)(.*?)(\*\*\*\*\*)\s''', re.DOTALL)
+re5 = re.compile('\s(_______________)(.*?)(_______________)\s', re.DOTALL)
+re6 = re.compile('\n -.*')
+re7 = re.compile('\n(\s)*\d.*')
+re8 = re.compile('(\n( )*(\w)+( )*\n )|(\n( )*(\w)+(\s)+(\w)+( )*\n)|(\n( )*(\w)+(\s)+(\w)+(\s)+(\w)+( )*\n)')
 
 
-text = open('example_text.txt').read()
+text = open('lewis-a_23.').read()
 
 print(text)
 text = re.sub(re1, ' ', text)
 text = re.sub(re2, ' ', text)
 text = re.sub(re3, ' ', text)
-#for i in range(0,3):
-#    text = re.sub('re'+str(i+1), ' ', text)
+text = re.sub(re4, ' ', text)
+text = re.sub(re5, ' ', text)
+text = re.sub(re6, ' ', text)
+text = re.sub(re7, ' ', text)
+text = re.sub(re8, ' ', text)
+
 print('##############################################################')    
 print(text)
 
-docs = []
-
-chdir('/home/peter/Downloads/enron')
-# For each user we extract all the emails in their inbox
-
-names = [i for i in listdir('/home/peter/Downloads/enron')]
-for name in names:
-    sent = '/home/peter/Downloads/enron/' + str(name) + '/sent'   
-    try: 
-        chdir(sent)     
-        for email in listdir(sent):
-            text = open(email,'r').read()
-            
-            # Regular expressions are used below to remove 'clutter'
-            text = re.sub(re1,' ',text)
-            text = re.sub(re2,' ',text)
-            text = re.sub(re3,'',text)
-            text = re.sub(re4,' ',text)
-            docs.append(text)           
-    except:
-        pass
